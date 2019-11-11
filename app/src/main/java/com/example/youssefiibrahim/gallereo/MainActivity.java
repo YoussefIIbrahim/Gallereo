d04a6a3918806b6e672ad8b3d6a3c6a5d6793b1a
youssefiibrahim <youssefibrahim.guc@gmail.com>
2019-11-11 14:37:24 +0100

    Added the read storage permission

--- app/src/main/java/com/example/youssefiibrahim/gallereo/MainActivity.java
+++ app/src/main/java/com/example/youssefiibrahim/gallereo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.youssefiibrahim.gallereo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int READ_EXTERNAL_STORAGE_PERMISSION_RESULT = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMISSION_RESULT:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Call cursor loader
+                    Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkReadExternalStoragePermission();
+    }
+
+    private void checkReadExternalStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "The app needs to view the images on the device.", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                READ_EXTERNAL_STORAGE_PERMISSION_RESULT);
+            }
+        } else {
+
+        }
     }
 }
