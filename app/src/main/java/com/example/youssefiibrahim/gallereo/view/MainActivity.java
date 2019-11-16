147141e7105b6fe580221fb1af0113c31527eee5
youssefiibrahim <youssefibrahim.guc@gmail.com>
2019-11-16 22:08:42 +0100

    Cursor Loader (deprecated) and recyclerView

---
+++ app/src/main/java/com/example/youssefiibrahim/gallereo/view/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.youssefiibrahim.gallereo.view;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+
+import com.example.youssefiibrahim.gallereo.R;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final static int READ_EXTERNAL_STORAGE_PERMISSION_RESULT = 0;
+    private final static int MEDIASTORE_LOADER_ID = 0;
+    private RecyclerView memberRecyclerView;
+    private MediaStorageAdapter memberMediaStoreAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        memberRecyclerView = (RecyclerView) findViewById(R.id.thumbnailRecyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        memberRecyclerView.setLayoutManager(gridLayoutManager);
+
+        memberMediaStoreAdapter = new MediaStorageAdapter(this);
+        memberRecyclerView.setAdapter(memberMediaStoreAdapter);
+        checkReadExternalStoragePermission();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMISSION_RESULT:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Call cursor loader
+                    Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show();
+                    getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
+    }
+
+    private void checkReadExternalStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "The app needs to view the images on the device.", Toast.LENGTH_SHORT).show();
+                }
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                READ_EXTERNAL_STORAGE_PERMISSION_RESULT);
+            }
+        } else {
+            getSupportLoaderManager().initLoader(MEDIASTORE_LOADER_ID, null, this);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.MEDIA_TYPE
+        };
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + '='
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + '='
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+        return new CursorLoader(
+                this,
+                MediaStore.Files.getContentUri("external"),
+                projection,
+                selection,
+                null,
+                MediaStore.Files.FileColumns.DATE_ADDED + " DESC"
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+}
