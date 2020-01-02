aca605d63106af356b9697ff4c1009812303f35f
youssefiibrahim <youssefibrahim.guc@gmail.com>
2020-01-02 13:19:31 +0100

    Merging v4

--- app/src/main/java/com/example/youssefiibrahim/gallereo/view/MainActivity.java
+++ app/src/main/java/com/example/youssefiibrahim/gallereo/view/MainActivity.java
@@ -92,13 +92,13 @@ public class MainActivity extends AppCompatActivity
         }
         if (isPermissionGranted()) {
             if (ResponseWrapper.singleton != null) {
-                ArrayList<String> imagesToProcess = DataRW.getImagesPath(this); //DataRW.filterPaths(allPaths);
+                ArrayList<String> imagesToProcess = DataRW.filterPaths(allPaths);
                 ImageStructuresWrapper wrapper = DataRW.getImages(imagesToProcess);
                 System.out.println("imagesToProcess = " + imagesToProcess);
                 ProcessAndSaveThread thread = new ProcessAndSaveThread(imagesToProcess, this);
                 thread.start();
             } else {
-                ArrayList<String> imagesToProcess = DataRW.getImagesPath(this); //DataRW.filterPaths(allPaths);
+                ArrayList<String> imagesToProcess = DataRW.filterPaths(allPaths);
                 ImageStructuresWrapper wrapper = DataRW.getImages(imagesToProcess);
                 System.out.println("imagesToProcess = " + imagesToProcess);
                 ProcessAndSaveThread thread = new ProcessAndSaveThread(imagesToProcess, this);
